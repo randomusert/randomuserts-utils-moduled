fc9622227c9a095da9df9773c0cd8ac1bd7487e4
randomusert <eetupajamaki@gmail.com>
2026-07-28 14:17:17 +0300

    Update mixin

--- fabric/src/main/java/io/github/randomusert/mods/randomuserts_utils/fabric/mixin/MainMenuAddCreateTestWorld.java
+++ fabric/src/main/java/io/github/randomusert/mods/randomuserts_utils/fabric/mixin/MainMenuAddCreateTestWorld.java
@@ -2,24 +2,20 @@ package io.github.randomusert.mods.randomuserts_utils.fabric.mixin;
 
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.SharedConstants;
-import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
-import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-
 @Mixin(TitleScreen.class)
-public class MainMenuAddCreateTestWorld extends Screen {
-    protected MainMenuAddCreateTestWorld(Component title) {
-        super(title);
-    }
+public class MainMenuAddCreateTestWorld {
 
     @Inject(at = @At("RETURN"), method = "createTestWorldButton")
     private void forceCreateTestWorldButton(int topPos, int spacing, CallbackInfoReturnable<Integer> cir) {
-        // Check if the env is a Dev env and Set IS_RUNNING_IN_IDE just for creating the create test button
+        // Check if the env is a Dev env and Set IS_RUNNING_IN_IDE just for creating the create test button.
+        // Technicly even if the mod is in a dev env, it technicly could be something else.
+        // Later We should introduce a proper check for IDE running, but for now we can do this.
         if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
             SharedConstants.IS_RUNNING_IN_IDE = true;
         }
