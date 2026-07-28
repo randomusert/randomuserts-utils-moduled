3284dbcd9acac0b27cc05534243f0adc877d3447
randomusert <eetupajamaki@gmail.com>
2026-07-28 10:08:44 +0300

    Force a create test world button

--- fabric/src/main/java/io/github/randomusert/mods/randomuserts_utils/fabric/mixin/MainMenuAddCreateTestWorld.java
+++ fabric/src/main/java/io/github/randomusert/mods/randomuserts_utils/fabric/mixin/MainMenuAddCreateTestWorld.java
@@ -1,18 +1,14 @@
 package io.github.randomusert.mods.randomuserts_utils.fabric.mixin;
 
-import io.github.randomusert.mods.randomuserts_utils.api.LoggingHelper;
-import io.github.randomusert.mods.randomuserts_utils.fabric.utils.EnvChecker;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.gui.components.Button;
+import net.minecraft.SharedConstants;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
-import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.level.levelgen.presets.WorldPresets;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
 @Mixin(TitleScreen.class)
@@ -21,19 +17,11 @@ public class MainMenuAddCreateTestWorld extends Screen {
         super(title);
     }
 
-    @Inject(at = @At("RETURN"), method = "init")
-    private void addCustomButton(CallbackInfo ci) {
-        boolean isDevEnv = FabricLoader.getInstance().isDevelopmentEnvironment();
-
-        if (isDevEnv) {
-            this.addRenderableWidget(
-                    Button.builder(Component.literal("Create Test World"), button -> {
-                                LoggingHelper.info("Hello from the button in the main menu");
-                            })
-                            .bounds(10, this.height - 30, 200, 20)
-                            .build()
-            );
+    @Inject(at = @At("RETURN"), method = "createTestWorldButton")
+    private void forceCreateTestWorldButton(int topPos, int spacing, CallbackInfoReturnable<Integer> cir) {
+        // Check if the env is a Dev env and Set IS_RUNNING_IN_IDE just for creating the create test button
+        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
+            SharedConstants.IS_RUNNING_IN_IDE = true;
         }
-
     }
 }
